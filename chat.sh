fc1d7194be11574b97567df618f92e9ff613aa15
Yingyue-L <1265170408@qq.com>
2024-05-13 10:56:17 +0800

    ✨ feat: lvis

--- chat.sh
+++ chat.sh
@@ -6,26 +6,28 @@ IFS=',' read -ra GPULIST <<< "$gpu_list"
 CHUNKS=${#GPULIST[@]}
 
 
-# for IDX in $(seq 0 $((CHUNKS-1))); do
-#     CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m chat \
-#         --split val \
-#         --num-chunks $CHUNKS \
-#         --chunk-idx $IDX &
-# done
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m chat \
+        --json-path lvis/lvis_v1_val.json \
+        --image-path coco/train2017 \
+        --output-path lvis/lvis_v1_val_phrase.jsonl \
+        --type lvis \
+        --num-chunks $CHUNKS \
+        --chunk-idx $IDX &
+done
 
-# wait
+wait
 
-# output_file=./coco_segm_text/val/panoptic_phrase.jsonl
+output_file=./lvis/lvis_v1_val_phrase.jsonl
 
-# # Clear out the output file if it exists.
-# > "$output_file"
+# Clear out the output file if it exists.
+> "$output_file"
 
-# # Loop through the indices and concatenate each file.
-# for IDX in $(seq 0 $((CHUNKS-1))); do
-#     cat ./coco_segm_text/val/panoptic_phrase_${CHUNKS}_${IDX}.jsonl >> "$output_file"
-# done
+# Loop through the indices and concatenate each file.
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    cat ./lvis/lvis_v1_val_phrase_${CHUNKS}_${IDX}.jsonl >> "$output_file"
+done
 
-# wait
 
 
 # for IDX in $(seq 0 $((CHUNKS-1))); do
@@ -37,17 +39,17 @@ CHUNKS=${#GPULIST[@]}
 
 # wait
 
-output_file=./coco_segm_text/test/panoptic_phrase.jsonl
+# output_file=./lvis/panoptic_phrase.jsonl
 
-# Clear out the output file if it exists.
-> "$output_file"
+# # Clear out the output file if it exists.
+# > "$output_file"
 
-# Loop through the indices and concatenate each file.
-for IDX in $(seq 0 $((CHUNKS-1))); do
-    cat ./coco_segm_text/test/panoptic_phrase_${CHUNKS}_${IDX}.jsonl >> "$output_file"
-done
+# # Loop through the indices and concatenate each file.
+# for IDX in $(seq 0 $((CHUNKS-1))); do
+#     cat ./coco_segm_text/test/panoptic_phrase_${CHUNKS}_${IDX}.jsonl >> "$output_file"
+# done
 
-wait
+# wait
 
 # for IDX in $(seq 0 $((CHUNKS-1))); do
 #     CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m chat \
